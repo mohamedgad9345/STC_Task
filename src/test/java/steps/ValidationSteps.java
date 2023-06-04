a38536dceebde12920e16acf647eb5eb721a69f0
Mohamed Gad <117829989+mohamedgad9345@users.noreply.github.com>
2023-06-04 15:27:24 +0300

    Update ValidationSteps.java
    updated

--- src/test/java/steps/ValidationSteps.java
+++ src/test/java/steps/ValidationSteps.java
@@ -62,110 +62,66 @@ public class ValidationSteps extends TestBase {
 
     }
 
-        @Then("I should see the subscription package details for Egypt")
-    public void i_should_see_subscription_packages(DataTable dataTable) {
-        List<Map<String, String>> data = dataTable.asMaps(String.class, String.class);
-        for (Map<String, String> row : data) {
-            String planName = row.get("plan-name");
-            String price = row.get("price");
-            String currency = row.get("Currency");
-            String expectedText = String.format("%s %s %s", planName, price, currency);
-//            String expectedText = String.format("%s", planName);
-//            String expectedText1 = String.format("%s", price);
-//            String expectedText2 = String.format("%s", currency);
-            List<String> actualPackageDetails = new ArrayList<String>();
-            actualPackageDetails.add(planName + price + currency);
+   @Then("I can assert on all details like bundleType and price and currency for {string}")
+   public void i_can_assert_on_all_details_like_bundle_type_and_price_and_currency_for(String country) {
+       WebElement bundleLite = driver.findElement(planLiteElement);
+       WebElement priceLite = driver.findElement(priceLiteElement);
+       WebElement currencyLite = driver.findElement(currencyLiteElement);
+       WebElement bundleBasic = driver.findElement(planBasicElement);
+       WebElement priceBasic = driver.findElement(priceBasicElement);
+       WebElement currencyBasic = driver.findElement(currencyBasicElement);
+       WebElement bundlePrem = driver.findElement(planPremElement);
+       WebElement pricePrem = driver.findElement(pricePremElement);
+       WebElement currencyPrem = driver.findElement(currencyPremElement);
 
-            WebElement element = driver.findElement(planLiteElement);
-            WebElement element1 = driver.findElement(priceLiteElement);
-            WebElement element2 = driver.findElement(currencyLiteElement);
+       Assert.assertTrue(bundleLite.getText().equals("لايت"));
+       System.out.println(bundleLite.getText());
+       if(country.equals("Egypt")) {
+           Assert.assertTrue(priceLite.getText().equals("0.25"));
+           System.out.println(priceLite.getText());
+       } else if (country.equals("UAE")) {
+           Assert.assertTrue(priceLite.getText().equals("5.4"));
+           System.out.println(priceLite.getText());
+       } else if (country.equals("Palestine")) {
+           Assert.assertTrue(priceLite.getText().equals("2.4"));
+           System.out.println(priceLite.getText());
+       }
+       Assert.assertTrue(currencyLite.getText().equals("دولار أمريكي/شهر"));
+       System.out.println(currencyLite.getText());
+       Assert.assertTrue(bundleBasic.getText().equals("الأساسية"));
+       System.out.println(bundleBasic.getText());
+       if(country.equals("Egypt")) {
+           Assert.assertTrue(priceBasic.getText().equals("0.5"));
+           System.out.println(priceBasic.getText());
+       } else if (country.equals("UAE")) {
+           Assert.assertTrue(priceBasic.getText().equals("10.9"));
+           System.out.println(priceBasic.getText());
+       } else if (country.equals("Palestine")) {
+           Assert.assertTrue(priceBasic.getText().equals("4.8"));
+           System.out.println(priceBasic.getText());
+       }
+       Assert.assertTrue(currencyBasic.getText().equals("دولار أمريكي/شهر"));
+       System.out.println(currencyBasic.getText());
+       Assert.assertTrue(bundlePrem.getText().equals("بريميوم"));
+       System.out.println(bundlePrem.getText());
+       if(country.equals("Egypt")) {
+           Assert.assertTrue(pricePrem.getText().equals("1"));
+           System.out.println(pricePrem.getText());
+       } else if (country.equals("UAE")) {
+           Assert.assertTrue(pricePrem.getText().equals("16.3"));
+           System.out.println(pricePrem.getText());
+       } else if (country.equals("Palestine")) {
+           Assert.assertTrue(pricePrem.getText().equals("7.2"));
+           System.out.println(pricePrem.getText());
+       }
+       Assert.assertTrue(currencyPrem.getText().equals("دولار أمريكي/شهر"));
+       System.out.println(currencyPrem.getText());
+   }
 
-            String actualText = element.getText();
-            String actualTex1 = element1.getText();
-            String actualText2 = element2.getText();
-            Assert.assertEquals(expectedText, actualText);
-//            Assert.assertEquals(expectedText1, actualTex1);
-//            Assert.assertEquals(expectedText2, actualText2);
-            List<String>[] subscriptionPackages = new List[0];
-            for (List<String> packageDetails : subscriptionPackages) {
-                if (packageDetails.get(0).equals(planLiteElement)) {
-                    Assert.assertEquals(Double.parseDouble(packageDetails.get(1)), priceLiteElement);
-                    Assert.assertEquals(packageDetails.get(2), currencyLiteElement);
-                    return;
-                }
-            }
-        }
-
-        List<String>[] subscriptionPackages = new List[0];
-        for (List<String> packageDetails : subscriptionPackages) {
-            if (packageDetails.get(0).equals(planLiteElement)) {
-                Assert.assertEquals(Double.parseDouble(packageDetails.get(1)), priceLiteElement);
-                Assert.assertEquals(packageDetails.get(2), currencyLiteElement);
-                return;
-            }
-        }
-
-
-//    @Then("I can assert on all details like bundleType and price and currency for {string}")
-//    public void i_can_assert_on_all_details_like_bundle_type_and_price_and_currency_for(String country) {
-//        WebElement bundleLite = driver.findElement(planLiteElement);
-//        WebElement priceLite = driver.findElement(priceLiteElement);
-//        WebElement currencyLite = driver.findElement(currencyLiteElement);
-//        WebElement bundleBasic = driver.findElement(planBasicElement);
-//        WebElement priceBasic = driver.findElement(priceBasicElement);
-//        WebElement currencyBasic = driver.findElement(currencyBasicElement);
-//        WebElement bundlePrem = driver.findElement(planPremElement);
-//        WebElement pricePrem = driver.findElement(pricePremElement);
-//        WebElement currencyPrem = driver.findElement(currencyPremElement);
-//
-//        Assert.assertTrue(bundleLite.getText().equals("لايت"));
-//        System.out.println(bundleLite.getText());
-//        if(country.equals("Egypt")) {
-//            Assert.assertTrue(priceLite.getText().equals("0.25"));
-//            System.out.println(priceLite.getText());
-//        } else if (country.equals("UAE")) {
-//            Assert.assertTrue(priceLite.getText().equals("5.4"));
-//            System.out.println(priceLite.getText());
-//        } else if (country.equals("Palestine")) {
-//            Assert.assertTrue(priceLite.getText().equals("2.4"));
-//            System.out.println(priceLite.getText());
-//        }
-//        Assert.assertTrue(currencyLite.getText().equals("دولار أمريكي/شهر"));
-//        System.out.println(currencyLite.getText());
-//        Assert.assertTrue(bundleBasic.getText().equals("الأساسية"));
-//        System.out.println(bundleBasic.getText());
-//        if(country.equals("Egypt")) {
-//            Assert.assertTrue(priceBasic.getText().equals("0.5"));
-//            System.out.println(priceBasic.getText());
-//        } else if (country.equals("UAE")) {
-//            Assert.assertTrue(priceBasic.getText().equals("10.9"));
-//            System.out.println(priceBasic.getText());
-//        } else if (country.equals("Palestine")) {
-//            Assert.assertTrue(priceBasic.getText().equals("4.8"));
-//            System.out.println(priceBasic.getText());
-//        }
-//        Assert.assertTrue(currencyBasic.getText().equals("دولار أمريكي/شهر"));
-//        System.out.println(currencyBasic.getText());
-//        Assert.assertTrue(bundlePrem.getText().equals("بريميوم"));
-//        System.out.println(bundlePrem.getText());
-//        if(country.equals("Egypt")) {
-//            Assert.assertTrue(pricePrem.getText().equals("1"));
-//            System.out.println(pricePrem.getText());
-//        } else if (country.equals("UAE")) {
-//            Assert.assertTrue(pricePrem.getText().equals("16.3"));
-//            System.out.println(pricePrem.getText());
-//        } else if (country.equals("Palestine")) {
-//            Assert.assertTrue(pricePrem.getText().equals("7.2"));
-//            System.out.println(pricePrem.getText());
-//        }
-//        Assert.assertTrue(currencyPrem.getText().equals("دولار أمريكي/شهر"));
-//        System.out.println(currencyPrem.getText());
-//    }
-
-//    @Then("close the website")
-//    public void close_the_website() {
-//        stopDriver();
-//    }
+   @Then("close the website")
+   public void close_the_website() {
+       stopDriver();
+   }
 
 }
 }
